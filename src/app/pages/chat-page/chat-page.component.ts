39d6d89117ec882bb579f53af0a29d991c6868e3
ZairaCabrera <127335893+ZairaCabrera@users.noreply.github.com>
2025-07-03 12:29:49 +0200

    feature(chat scss, component): ajustar el scrolltobutton
    crear el css de la interfaz del chat, ahora se muestran los globos de mensajes enviados a la derecha y recibidos a la izquierda, se ajusta el header, content y el footer.
    Estoy trabajando en el scrolltobutton para llevar el scroll hacia abajo.

--- src/app/pages/chat-page/chat-page.component.ts
+++ src/app/pages/chat-page/chat-page.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { IonicModule, NavController } from '@ionic/angular';
+import { Component, ElementRef, inject, OnInit, signal, ViewChild, AfterViewChecked } from '@angular/core';
+import { IonicModule, NavController, IonContent } from '@ionic/angular';
 import { AuthService } from 'src/app/services/auth.service';
 import { logOutOutline } from 'ionicons/icons';
 import { CommonModule, DatePipe } from '@angular/common';
@@ -21,11 +21,14 @@ export default class ChatPageComponent implements OnInit {
   private navCtrl = inject(NavController);
 
   public logOutOutline = logOutOutline; //icono de cerrar sesión
-
   public messages = signal<Messages[]>([]);//aqui se crean los mensajes con la interfaz que queremos
   currentUser = this.auth.currentUser$ ; //info usuario autenticado: observable<User|null>
   public messageForm!: FormGroup; //creamos el formulario
 
+  //bandera dispara el scroll
+  private shouldScroll = false;
+  //capturamos el elemento del DOM chatlist
+   @ViewChild(IonContent, { static: false }) content!: IonContent;
 
   constructor(private fb: FormBuilder,private chatService: ChatService, private authService: AuthService) {
     this.messageForm = this.fb.group({
@@ -36,7 +39,20 @@ export default class ChatPageComponent implements OnInit {
   ngOnInit() {
     this.chat.getMessages().subscribe(msgs => {
       this.messages.set(msgs);
+      // marcamos scroll tras llegada de nuevos mensajes
+      this.shouldScroll = true;
     });
+  }
+
+  ngAfterViewChecked() {
+    if (this.shouldScroll) {
+      this.shouldScroll = false;
+      this.scrollToBottom();
+    }
+  }
+
+  private scrollToBottom() {
+    this.content.scrollToBottom(300);
   }
 
   // llamamos a chat.sendMessage() y luego reseteamos el formulario
@@ -55,13 +71,17 @@ export default class ChatPageComponent implements OnInit {
       user:   currentUser.displayName || 'Anónimo',
       from:   currentUser.uid,
       text,
-      ts:     Date.now(),      // o serverTimestamp() si usas la API modular
+      ts:     Date.now(),
       avatar: currentUser.photoURL ?? undefined
     };
 
     try {
       await this.chatService.addMessage(newMsg);
       this.messageForm.reset();
+
+      // marcamos la bandera para que en AfterViewChecked haga scroll
+      this.shouldScroll = true;
+
     } catch (err) {
       console.error('Error añadiendo mensaje:', err);
     }
